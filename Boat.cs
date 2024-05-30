cce4dc6b3afd718488d494c8c218539c70d13a37
michalkopera1 <147231200+michalkopera1@users.noreply.github.com>
2024-05-30 19:44:35 +0200

    Add files via upload

---
+++ Boat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary1;
+
+namespace VehiclesLibrary
+{
+    public class Boat : EnginePoweredVehicle, IVehicle, ISailable
+    {
+        private readonly int _buoyancy;
+        private MovingModule _MovingModule;
+        public string Name => GetType().Name;
+        public int Buoyancy => _buoyancy;
+        public Boat(int horsePower, int buoyancy) : base(horsePower, Engine.FuelType.Diesel)
+        {
+            ActualEnvironment = Environments.Sailing;
+            AvailableEnvironments.Add(Environments.Sailing);
+            _buoyancy = buoyancy;
+            _MovingModule = new MovingModule(buoyancy, true);
+        }
+
+        public void Accelerate(double targetSpeed)
+        {
+            _MovingModule.TryToAccelerate(ActualEnvironment, ref _state, ref MovingSpeed, targetSpeed, Name);
+        }
+
+        public void LeaveWater()
+        {
+            Console.WriteLine($"{Name} can not leave water environment.");
+        }
+
+        public void Sail()
+        {
+            Console.WriteLine($"{Name} is already sailing.");
+        }
+
+        public void SlowDown(double targetSpeed)
+        {
+            _MovingModule.TryToSlowDown(ActualEnvironment, ref _state, ref MovingSpeed, targetSpeed, Name);
+        }
+
+        public void StopVehicle()
+        {
+            _MovingModule.StopMoving(ref _state, ActualEnvironment, ref MovingSpeed, Name);
+        }
+        public override string ToString()
+        {
+            return $"{Name}" + base.ToString() + $"\nBuoyancy: {Buoyancy}\n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+using System.Linq;
+using VehiclesLibrary;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicleList = new List<Vehicle>();
+
+            Plane boeing = new Plane(300, Engine.FuelType.Petrol); //samolot
+
+            Car ford = new Car(100, Engine.FuelType.Petrol); // auto 
+
+            Boat arka = new Boat(10000, 20000);  // lodz
+
+            Amphibian amphibian = new Amphibian(200, 500);  // amfibia
+
+            vehicleList.Add(boeing);  // samolot 
+
+            vehicleList.Add(ford);  // auto 
+
+            vehicleList.Add(arka);   // lodz
+
+            vehicleList.Add(amphibian); // amfibia
+
+            
+            foreach(var v in vehicleList)
+                Console.WriteLine(v);
+
+            Console.WriteLine("-Onground vehicles-\n");
+
+            var groundVehicles = vehicleList.Where(vehicle => vehicle.actualEnv == Environments.OnGround);
+
+            foreach(var v in groundVehicles)
+                Console.WriteLine(v);
+
+            Console.WriteLine("-By Speed ascending-\n");
+
+            boeing.Accelerate(100);
+            boeing.Fly();                   // samolot
+            boeing.Accelerate(150);
+
+            ford.Accelerate(150);  // auto 
+
+            arka.Accelerate(40);            //lodz
+
+            amphibian.Accelerate(100);  // amfibia 
+            amphibian.Sail();
+
+
+            Console.WriteLine();
+            var bySpeedAsc = vehicleList.OrderBy(vehicle => Vehicle.UnitConverter(vehicle.ActualSpeed, vehicle.actualUnit, SpeedUnits.KMpH));
+            foreach(var v in bySpeedAsc)
+                Console.WriteLine(v);
+
+            Console.WriteLine("-Onground vehicles orderder by speed descending-");
+
+            var groundByDesc = vehicleList.Where(vehicle => vehicle.actualEnv == Environments.OnGround).OrderByDescending(vehicle => Vehicle.UnitConverter(vehicle.ActualSpeed, vehicle.actualUnit, SpeedUnits.KMpH));
+            foreach (var v in groundByDesc)
+                Console.WriteLine(v);
+
+           
+        }
+    }
+}
